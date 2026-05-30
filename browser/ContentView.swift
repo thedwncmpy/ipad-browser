1b5f442656d5614cb8eef29ab78f35e60b85fa4f
thedwncmpny-mini <mail@edwinolivaers.com>
2026-05-29 23:53:15 -0700

    first commit

---
+++ browser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  browser
+//
+//  Created by Edwin Olivares on 5/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ browser/browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  browserApp.swift
+//  browser
+//
+//  Created by Edwin Olivares on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
